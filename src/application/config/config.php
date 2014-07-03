58876cdb759eb575bb84a1b6eacb87674f7a13ad
Vo Ngoc Trieu <trieugia008@gmail.com>
2014-07-03 22:56:27 +0700

    Add amz to project

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/amz/';
+$config['base_url']	= 'http://amz.com/';
 
 /*
 |--------------------------------------------------------------------------
